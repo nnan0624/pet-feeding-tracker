703f549d01845417f8bfaf0bc3b741b232dc4143
nnan0624 <153707817+nnan0624@users.noreply.github.com>
2025-08-27 08:02:21 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PetFeedingTracker from './components/PetFeedingTracker';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PetFeedingTracker />
+    </div>
+  );
+}
+
+export default App;
